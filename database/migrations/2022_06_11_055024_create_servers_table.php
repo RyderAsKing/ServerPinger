916834a82299f16c446acbb443f3bd30ccba063d
RyderAsking <clasherlodt@gmail.com>
2022-06-11 11:59:11 +0530

    changed name from not required to required

--- database/migrations/2022_06_11_055024_create_servers_table.php
+++ database/migrations/2022_06_11_055024_create_servers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration {
     {
         Schema::create('servers', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->nullable();
+            $table->string('name');
             $table->string('ip');
             $table->string('port');
             $table->string('status')->default('offline');
